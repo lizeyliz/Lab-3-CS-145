35c8716c56b61640eb8368b687b410cca62dc6af
lizeyliz <alizaeaton@gmail.com>
2024-10-09 09:57:25 -0700

    adding comments

--- LetterInventory.java
+++ LetterInventory.java
@@ -1,11 +1,16 @@
 public class LetterInventory {
     public static void main(String args[]){
         //first method
+        // Array for counting each letter: position 0 is a, 25 is z, etc.
         int[] letterCount = new int[25];
+        //test string, will add user input later
         String test = "Hello";
+        //runs through each char in string
         for (int i=0; i < test.length(); i++) {
+            //getting the ASCII value of the character (lowercase)
             int number = (int) test.toLowerCase().charAt(i);
-            System.out.println(number);
+            System.out.println(number); //test code
+            //lowercase ASCII alphabet
             if(number < 124 && number > 96){
                 //add to lettercount
             } else {
