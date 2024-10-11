9ba8fa3c14815319134ffda25c471418bd5cf350
R.M.McKernan <WhatLibrarian@users.noreply.github.com>
2024-10-10 17:12:59 -0700

    for loop for char count. 26.

--- LetterInventory.java
+++ LetterInventory.java
@@ -1,12 +1,16 @@
+import java.net.SocketImpl;
 import java.util.Scanner;
+
 public class LetterInventory {
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
         //first METHOD: letterInventory
         // Array for counting each letter: position 0 is a, 25 is z, etc.
-        int[] letterCount = new int[25];
+        // Wasn't working for 'Zebra'. Changed to 26, it works but not sure why
+        // Will follow up with Darrell to explain valid vs. out-of-bounds indices
+        int[] letterCount = new int[26];
         //user input
-        System.out.println("Enter a string:");
+        System.out.println("Enter a word or a short phrase and we'll give you a count of A-Z characters:");
         String userString = scan.nextLine();
         //runs through each char in string
         for (int i=0; i < userString.length(); i++) {
@@ -44,11 +48,15 @@ public class LetterInventory {
                 case 122 -> letterCount[25]++; //z
             } //end switch/case
         } //end for loop (run through string)
-        //letter count display
-        System.out.println("Letter Count:");
-        System.out.println("a: " + letterCount[0]);
-        System.out.println("b: " + letterCount[1]);
-        //etc
+        
+        //letter count display for a-z
+        //System.out.println("Letter Count:");
+        //System.out.println("a: " + letterCount[0]);
+       System.out.println("b: " + letterCount[1]);
+       for (int row = 0; row < 26; row++) {  //iterate over the array, prints final count of each letter from a-z
+        System.out.println((char)(row + 'a') + ": " + letterCount[row]);
+       }
+        
     
     }//end method main
 }//end Class LetterInventory
